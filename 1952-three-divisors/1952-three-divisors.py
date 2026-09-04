166636216a4e2ca9da888c546fdb50e81fa7efa5
Abhay pandit <abhaytech12cy@gmail.com>
2026-09-04 15:12:20 +0530

    Time: 0 ms (100%), Space: 12.4 MB (88.53%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.py
+++ 1952-three-divisors/1952-three-divisors.py
@@ -1,11 +1,15 @@
 class Solution(object):
     def isThree(self, n):
+        import math
 
         num=n
-        sum=0
-        for i in range(1,num+1):
+        lst=[]
+        for i in range(1,int(math.sqrt(num))+1):
             if num%i==0: 
-                sum=sum+1
+                lst.append(i)
+                if i != num//i:
+                    lst.append(n//i)
+        sum=len(lst)            
         if sum == 3 :
             return True
         else :
